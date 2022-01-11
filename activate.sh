6d8ef3fadb79b60af0b0e28ce4189caebc0167f1
Andrei Chenchik <andrei@chenchik.me>
2022-01-11 15:36:55 +0100

    Rewrite activate.sh

--- activate.sh
+++ activate.sh
@@ -1,35 +1,80 @@
-for app in ${env_wrapped[@]}; do
-  eval "
-    ${app}() {
-      appPath=\$(which -a \$0 | grep \"^/\" | head -1)
+supported_shells=("zsh bash")
 
-      if [[ -z \$appPath ]]
-      then
-        echo \"\$0 not found\" 
-      else
-        if [[ -z \$OP_HOST || -z \$OP_VAULT || -z \$OP_TAG ]]
-        then
-          eval \"\$appPath \$@\"
-        else 
-          hostShorthand=\$(op signin --list | grep https://\$OP_HOST | head -1 | awk '{print \$2}')
-          if [[ -z \$hostShorthand ]]
-          then
-            echo \"Plase sign in into \$OP_HOST first\"
-            echo \"Run 'op sigin --help' for more info\"
-          else
-            sessionRef=\"OP_SESSION_\$hostShorthand\"
-            sessionKey=\${(P)sessionRef}
-            
-            if newKey=\$(op signin \$OP_HOST --session \${sessionKey:-expired})
-            then
-              eval \$newKey
-              envs=\$(op list items --vault \${OP_VAULT} --tags \${OP_TAG}/\$0 | jq 'sort_by(.overview.title)' | op get item - | jq '.details.sections[] | select( has(\"fields\") ) | .fields[] | {name: .t, value: .v}' | jq -r '\"\(.name)=\(.value)\"' | tr '\n' ' ')
+if test -n "$ZSH_VERSION"; then
+  SHELL_NAME=zsh
+elif test -n "$BASH_VERSION"; then
+  SHELL_NAME=bash
+fi
 
-              eval \"\${envs:0:-1} \$appPath \$@\"
-            fi
-          fi
-        fi
-      fi
-    }
-  "
-done
+export ENVINJ_SHELL=$SHELL_NAME
+
+if [[ ! " ${supported_shells[*]} " =~ " ${ENVINJ_SHELL} " ]]; then
+	echo "EJ: Can run only in bash or zsh environment"
+	return
+fi
+
+command_preexec="$(command -v preexec 2>/dev/null)"
+
+if [ "$command_preexec" != "" ] && [ "$ENVINJ_SHELL" = "zsh" ]; then
+	echo "EJ: Can't run in zsh environment that already leverage preexec command."
+	return
+fi
+
+
+validate_command () {
+	if [ "$1" = "" ]; then
+		return
+	fi
+
+	for basename in $(basename $1); do
+		for skipname in "$ENVINJ_SKIP"; do
+			if [ "$basename" = "$skipname" ]; then
+				envinj_skipping="yes"
+			fi
+		done
+
+		if [ "$envinj_skipping" = "yes" ]; then
+			continue
+		fi
+
+		for appname in "$ENVINJ_APPS"; do
+			if [ "$basename" = "$appname" ]; then
+				envinj_found="yes"
+				envinj_app="$basename"
+				break
+			fi
+		done
+
+		if [ "$envinj_found" = "yes" ]; then
+			break
+		fi
+	done
+
+	echo $envinj_app
+}
+
+preexec () {
+	if [ "$ENVINJ_PROVIDER" = "" ] && [ "$ENVINJ_APPS" = "" ]; then
+		return
+	fi
+
+	if [ "$ENVINJ_SHELL" = "bash" ]; then
+		command=$BASH_COMMAND
+	else
+		command=$1
+	fi
+
+	echo $command
+
+	ENVINJ_APP=$(validate_command $command)
+
+	if [ "$ENVINJ_APP" != "" ]; then
+		echo "EJ: Injecting environment variables for $ENVINJ_APP"
+		eval "$ENVINJ_PROVIDER $ENVINJ_APP"
+		echo "EJ: Vars set"
+	fi
+}
+
+if [ "$ENVINJ_SHELL" = "bash" ]; then
+	trap 'preexec' DEBUG
+fi
